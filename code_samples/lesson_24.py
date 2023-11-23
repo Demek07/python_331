d3568a747f39ed369a086e3f1ebd3ad6890253ed
V <v@yandex.ru>
2023-11-23 22:11:18 +0600

    lesson_24: bad_commit

--- code_samples/lesson_24.py
+++ code_samples/lesson_24.py
@@ -3,3 +3,10 @@ Lesson 24
 22.11.2023
 Повторение основных моментов связанных с Git и Github
 """
+
+def say_hello(name: str) -> None:
+    print(f'Привет {name}!')
+
+
+def say_bye(name: str) -> None:
+    print(f'Пока {name}!')
