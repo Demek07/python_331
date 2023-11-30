68b559919c665c9efbac643775a0094e274d8b82
V <v@yandex.ru>
2023-11-30 22:07:08 +0600

    lesson_26: приватные и защищенные атрибуты и методы. Синтаксис

--- code_samples/lesson_26.py
+++ code_samples/lesson_26.py
@@ -10,4 +10,48 @@ Lesson 26
 - чтобы скрыть сложную логику от пользователя
 - чтобы скрыть внутреннюю реализацию от пользователя
 
+Какие есть виды инкапсуляции?
+- public - публичные данные и методы
+- protected - защищенные данные и методы
+- private - приватные данные и методы
+
+Protected - это когда в начале имени атрибута или метода стоит один нижний подчеркивание
+Private - это когда в начале имени атрибута или метода стоит два нижних подчеркивания
 """
+
+
+class User:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self._protected = 'protected'
+        self.__private = 'private'
+
+    @staticmethod
+    def _protected_method():
+        print(f'Это защищенный метод')
+
+    @staticmethod
+    def __private_method():
+        print('Это приватный метод')
+
+    @staticmethod
+    def public_method():
+        print('Это публичный метод')
+
+
+# Создаем экземпляр класса
+user = User('Вася', 20)
+
+# Публичные атрибуты
+print(user.name)
+print(user.age)
+
+# Защищенные атрибуты
+print(user._protected)
+# print(user.__private) # Вызовет ошибку AttributeError: 'User' object has no attribute '__private'
+
+
+# Добудем имена всех атрибутов класса
+print(user.__dict__)
+print(user._User__private)
