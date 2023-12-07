a9c7d18503d4c44762b2ec46c8f3ece107434a2e
V <v@yandex.ru>
2023-12-07 22:51:50 +0600

    lesson_28: Абстрактные классы. Простой пример

--- code_samples/lesson_28.py
+++ code_samples/lesson_28.py
@@ -7,66 +7,78 @@ Lesson 28
 - Цепочка наследования
 - Множественное наследование
 - MRO - Method Resolution Order
+- Абстрактные классы
 """
 
+from abc import ABC, abstractmethod
 
-# Множественное наследование
-"""
-Что это такое?
-MRO — это список классов, который Python использует для определения порядка поиска методов и атрибутов. 
-Если вам нужно найти определённый метод или атрибут в классе, 
-Python будет искать его в порядке, заданном MRO.
 
-Зачем это нужно?
-Когда у вас есть множественное наследование, может возникнуть вопрос: если один и тот же метод 
-или атрибут определён в нескольких родительских классах, 
-откуда Python должен взять его? MRO помогает решить эту проблему, указывая чёткий порядок.
+class AbstractMatryoshka(ABC):
+    """
+    Абстрактный класс Матрешка.
+    Методы:
+    - open - открывает матрешку
+    - display_info - печатает информацию о матрешке
+    """
 
-Как это работает?
-Python использует алгоритм C3 Linearization для создания MRO. 
-Этот алгоритм обеспечивает, что порядок будет однозначным и консистентным.
- В общем случае, Python сначала ищет в самом классе, затем в его родителях, согласно порядку MRO.
- 
-"""
-class BigMattrioshka:
+    def __init__(self, color):
+        self.color = color
 
-    def __init__(self):
+    @abstractmethod
+    def open(self, *args, **kwargs):
+        """
+        Открывает матрешку
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def display_info(self, *args, **kwargs):
+        """
+        Печатает информацию о матрешке
+        :return:
+        """
+        pass
+
+    def get_color(self):
+        """
+        Получить цвет матрешки
+        :return:
+        """
+        return self.color
+
+
+# TypeError: Can't instantiate abstract class AbstractMatryoshka with abstract methods display_info, open
+# abstract_matryoshka = AbstractMatryoshka('red')
+
+class BigMatryoshka(AbstractMatryoshka):
+    """
+    Большая Матрешка.
+    Методы:
+    - open - открывает матрешку
+    - display_info - печатает информацию о матрешке
+    """
+
+    def __init__(self, color):
+        super().__init__(color)
         self.size = 'big'
 
     def open(self):
-        print(f'Opening the {self.size} mattrioshka')
+        """
+        Это может быть даже просто заглушка
+        Но без этого метода мы не сможем создать экземпляр класса
+        :return:
+        """
+        pass
+
+    def display_info(self):
+        """
+        Это может быть даже просто заглушка
+        Но без этого метода мы не сможем создать экземпляр класса
+        :return:
+        """
+        pass
 
 
-class MediumMattrioshka:
-
-        def __init__(self):
-            self.size = 'medium'
-
-        def open(self):
-            print(f'Opening the {self.size} mattrioshka')
-
-        def is_medium(self):
-            return self.size == 'medium'
-
-class Smallmatrrioska:
-
-        def __init__(self):
-            self.size = 'small'
-
-        def open(self):
-            print(f'Opening the {self.size} mattrioshka')
-
-        def is_small(self):
-            return self.size == 'small'
-
-
-class ComplectMattrioshka(BigMattrioshka, MediumMattrioshka, Smallmatrrioska):
-    pass
-
-# Проверяем работу класса
-mattrioshka = ComplectMattrioshka()
-mattrioshka.open()
-print(mattrioshka.is_small())
-print(mattrioshka.is_medium())
-print(mattrioshka.size)
-print(ComplectMattrioshka.__mro__)
+big_matryoshka = BigMatryoshka('red')
+print(big_matryoshka.get_color())
