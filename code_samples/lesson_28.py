30f3967fb2c151178b01931db0271126d4aca4b3
V <v@yandex.ru>
2023-12-07 22:23:16 +0600

    lesson_28: MRO и множественное наследование

--- code_samples/lesson_28.py
+++ code_samples/lesson_28.py
@@ -6,34 +6,67 @@ Lesson 28
 - Повторение материала про наследование
 - Цепочка наследования
 - Множественное наследование
+- MRO - Method Resolution Order
 """
 
 
-# Многоуровневое наследование
-class BigMattrioshka:
-    pass
-
-
-class MediumMattrioshka(BigMattrioshka):
-    pass
-
-
-class SmallMattrioshka(MediumMattrioshka):
-    pass
-
-
 # Множественное наследование
+"""
+Что это такое?
+MRO — это список классов, который Python использует для определения порядка поиска методов и атрибутов. 
+Если вам нужно найти определённый метод или атрибут в классе, 
+Python будет искать его в порядке, заданном MRO.
+
+Зачем это нужно?
+Когда у вас есть множественное наследование, может возникнуть вопрос: если один и тот же метод 
+или атрибут определён в нескольких родительских классах, 
+откуда Python должен взять его? MRO помогает решить эту проблему, указывая чёткий порядок.
+
+Как это работает?
+Python использует алгоритм C3 Linearization для создания MRO. 
+Этот алгоритм обеспечивает, что порядок будет однозначным и консистентным.
+ В общем случае, Python сначала ищет в самом классе, затем в его родителях, согласно порядку MRO.
+ 
+"""
 class BigMattrioshka:
-    pass
+
+    def __init__(self):
+        self.size = 'big'
+
+    def open(self):
+        print(f'Opening the {self.size} mattrioshka')
 
 
 class MediumMattrioshka:
-    pass
 
+        def __init__(self):
+            self.size = 'medium'
+
+        def open(self):
+            print(f'Opening the {self.size} mattrioshka')
+
+        def is_medium(self):
+            return self.size == 'medium'
 
 class Smallmatrrioska:
-    pass
+
+        def __init__(self):
+            self.size = 'small'
+
+        def open(self):
+            print(f'Opening the {self.size} mattrioshka')
+
+        def is_small(self):
+            return self.size == 'small'
 
 
 class ComplectMattrioshka(BigMattrioshka, MediumMattrioshka, Smallmatrrioska):
     pass
+
+# Проверяем работу класса
+mattrioshka = ComplectMattrioshka()
+mattrioshka.open()
+print(mattrioshka.is_small())
+print(mattrioshka.is_medium())
+print(mattrioshka.size)
+print(ComplectMattrioshka.__mro__)
