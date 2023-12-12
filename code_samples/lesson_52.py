39890ab67f7140da4d68b40eb1026b1dc15fc5ba
V <v@yandex.ru>
2023-12-12 22:39:06 +0600

    lesson_52: NotImplemented - ограничение. Теперь мы сравниваем только гири с гирями

--- code_samples/lesson_52.py
+++ code_samples/lesson_52.py
@@ -6,8 +6,9 @@ Lesson 52
 Dunder - double underscore
 
 __init__ - конструктор класса
-
+Как сравниваются строки
 Методы Сравнения
+NotImplemented - когда мы не можем сравнить объекты - возвращаем NotImplemented
 
     __eq__(self, other):
         Определяет поведение оператора равенства ==.
@@ -33,6 +34,10 @@ __init__ - конструктор класса
         Определяет поведение оператора "больше или равно" >=.
         Пример: def __ge__(self, other): ...
 
+    __str__(self):
+        Возвращает строковое представление объекта. Используется для пользовательского вывода.
+
+
 """
 
 
@@ -44,13 +49,16 @@ class Kettlebell:
         self.weight = weight
         self.color = color
 
+
     def __eq__(self, other):
         """
         Метод __eq__ определяет поведение оператора равенства ==.
         :param other:
         :return:
         """
-        return self.weight == other.weight
+        if isinstance(other, Kettlebell):
+            return self.weight == other.weight
+        return NotImplemented
 
     def __ne__(self, other):
         """
@@ -58,7 +66,9 @@ class Kettlebell:
         :param other:
         :return:
         """
-        return self.weight != other.weight
+        if isinstance(other, Kettlebell):
+            return self.weight != other.weight
+        return NotImplemented
 
     def __lt__(self, other):
         """
@@ -66,7 +76,8 @@ class Kettlebell:
         :param other:
         :return:
         """
-        return self.weight < other.weight
+        if isinstance(other, Kettlebell):
+            return self.weight < other.weight
 
     def __le__(self, other):
         """
@@ -74,7 +85,9 @@ class Kettlebell:
         :param other:
         :return:
         """
-        return self.weight <= other.weight
+        if isinstance(other, Kettlebell):
+            return self.weight <= other.weight
+        return NotImplemented
 
     def __gt__(self, other):
         """
@@ -82,7 +95,9 @@ class Kettlebell:
         :param other:
         :return:
         """
-        return self.weight > other.weight
+        if isinstance(other, Kettlebell):
+            return self.weight > other.weight
+        return NotImplemented
 
     def __ge__(self, other):
         """
@@ -90,7 +105,9 @@ class Kettlebell:
         :param other:
         :return:
         """
-        return self.weight >= other.weight
+        if isinstance(other, Kettlebell):
+            return self.weight >= other.weight
+        return NotImplemented
 
     def __str__(self):
         """
@@ -107,57 +124,20 @@ kettlebell_3 = Kettlebell(8, 'оранжевый')
 kettlebell_4 = Kettlebell(36, 'синий')
 kettlebell_5 = Kettlebell(16, 'бирюзовый')
 
-my_kettlebells = [kettlebell_1, kettlebell_2, kettlebell_3, kettlebell_4, kettlebell_5]
-
-# Сортировка списка kettlebells по весу
-# "print comprehension" - как list comprehension, только без создания списка
-[print(kettlebell) for kettlebell in my_kettlebells]
-my_kettlebells.sort()
-print(f'Сортировка по весу:')
-[print(kettlebell) for kettlebell in my_kettlebells]
-
-# Проверка приналежности объекта к классу с помощью isinstance()
-print(isinstance(kettlebell_1, Kettlebell))
-
-# Как сравниваются строки на примере метода __eq__
-
-string1 = 'Hello'
-string2 = 'hellO'
-
-print(string1 == string2)  # False
-print(string1 > string2)  # False
-print(string1 < string2)  # True
-
-# Печатаем индексы символов в строках
-[print(f'{string1[i]} - {ord(string1[i])}') for i in range(len(string1))]
-[print(f'{string2[i]} - {ord(string2[i])}') for i in range(len(string2))]
-
 """
-Сравнение строк в Python происходит лексикографически, что похоже на сравнение в словаре или телефонной книге. 
-При этом сравнении используется порядковый номер каждого символа в Unicode.
+В Python, NotImplemented — это специальное значение, которое может 
+быть возвращено некоторыми магическими методами для указания на то,
+что операция не поддерживается между данными типами. 
 
-Как Происходит Сравнение Строк:
+Особенно это актуально для методов сравнения и арифметических операций.
+Особенности NotImplemented:
 
-    Посимвольное Сравнение:
-        Сравнение начинается с первых символов двух строк.
-        Если первые символы равны, сравнение продолжается со следующими символами, и так далее.
+Не Исключение:
+    Важно отметить, что NotImplemented — это не исключение и не ошибка. 
+    Это специальное значение.
 
-    Остановка Сравнения:
-        Сравнение прекращается, когда найдены различные символы в соответствующих позициях.
-        Если одна строка является началом другой, более короткая строка считается "меньшей".
+Использование:
+    Обычно NotImplemented используется в магических методах, таких как
+     __eq__, __lt__, __add__ и других, чтобы указать, 
+     что операция не реализована для предоставленных типов аргументов.
 """
-
-index_list_1 = [ord(string1[i]) for i in range(len(string1))]
-index_list_2 = [ord(string2[i]) for i in range(len(string2))]
-
-# Сравнение по индексам в цикле, как только один из символов будет больше, цикл прервется
-
-for i in range(len(index_list_1)):
-    if index_list_1[i] > index_list_2[i]:
-        print(f'{string1[i]} > {string2[i]}')
-        break
-    elif index_list_1[i] < index_list_2[i]:
-        print(f'{string1[i]} < {string2[i]}')
-        break
-    else:
-        print(f'{string1[i]} = {string2[i]}')
