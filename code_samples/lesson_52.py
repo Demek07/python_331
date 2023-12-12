a6349bf48000e789d90292c219887ec67a773938
V <v@yandex.ru>
2023-12-12 22:47:29 +0600

    lesson_52: Сравнение у наследников. Это наследуется? Да!

--- code_samples/lesson_52.py
+++ code_samples/lesson_52.py
@@ -9,6 +9,7 @@ __init__ - конструктор класса
 Как сравниваются строки
 Методы Сравнения
 NotImplemented - когда мы не можем сравнить объекты - возвращаем NotImplemented
+Как работает сравнение у наследников
 
     __eq__(self, other):
         Определяет поведение оператора равенства ==.
@@ -45,10 +46,10 @@ class Kettlebell:
     """
     Класс гирь для занятий спортом
     """
+
     def __init__(self, weight: int, color: str):
         self.weight = weight
         self.color = color
-
 
     def __eq__(self, other):
         """
@@ -118,26 +119,33 @@ class Kettlebell:
         return f'Гиря. Цвет: {self.color}. Вес: {self.weight} кг.'
 
 
-kettlebell_1 = Kettlebell(16, 'черный')
-kettlebell_2 = Kettlebell(24, 'розовый')
-kettlebell_3 = Kettlebell(8, 'оранжевый')
-kettlebell_4 = Kettlebell(36, 'синий')
-kettlebell_5 = Kettlebell(16, 'бирюзовый')
+# Создаем наследника дизайнерскую гирю (добавляем атрибут модель)
 
-"""
-В Python, NotImplemented — это специальное значение, которое может 
-быть возвращено некоторыми магическими методами для указания на то,
-что операция не поддерживается между данными типами. 
+class DesignerKettlebell(Kettlebell):
+    """
+    Класс дизайнерских гирь для занятий спортом
+    """
 
-Особенно это актуально для методов сравнения и арифметических операций.
-Особенности NotImplemented:
+    def __init__(self, weight: int, color: str, model: str):
+        super().__init__(weight, color)
+        self.model = model
 
-Не Исключение:
-    Важно отметить, что NotImplemented — это не исключение и не ошибка. 
-    Это специальное значение.
+    def __str__(self):
+        """
+        Метод __str__ возвращает строковое представление объекта.
+        Используется для пользовательского вывода.
+        :return:
+        """
+        return f'Дизайнерская гиря. Модель: {self.model}. Цвет: {self.color}. Вес: {self.weight} кг.'
 
-Использование:
-    Обычно NotImplemented используется в магических методах, таких как
-     __eq__, __lt__, __add__ и других, чтобы указать, 
-     что операция не реализована для предоставленных типов аргументов.
-"""
+
+# Создаем наследника дизайнерскую гирю (добавляем атрибут модель)
+d_kettlebell_1 = DesignerKettlebell(16, 'синий', 'Классическая')
+d_kettlebell_2 = DesignerKettlebell(32, 'синий', 'С перламутровыми пуговицами')
+d_kettlebell_3 = DesignerKettlebell(8, 'синий', 'Без пуговиц')
+
+
+d_kettelbells = [d_kettlebell_1, d_kettlebell_2, d_kettlebell_3]
+[print(kettlebell) for kettlebell in d_kettelbells]
+d_kettelbells.sort()
+[print(kettlebell) for kettlebell in d_kettelbells]
