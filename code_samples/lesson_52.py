a8c94d9b81c513e0e772d2539147897447e2a10d
V <v@yandex.ru>
2023-12-12 23:10:16 +0600

    lesson_52: Практика. Класс город

--- code_samples/lesson_52.py
+++ code_samples/lesson_52.py
@@ -38,114 +38,30 @@ NotImplemented - когда мы не можем сравнить объекты
     __str__(self):
         Возвращает строковое представление объекта. Используется для пользовательского вывода.
 
+@total_ordering - декоратор, который позволяет определить все методы сравнения
+Но нам по прежнему надо описать хоть что-то из методов сравнения
 
+
+Для использования декоратора functools.total_ordering в Python необходимо определить,
+как минимум, два метода: __eq__
+
+и один из методов сравнения (__lt__, __le__, __gt__, или __ge__).
 """
+from functools import total_ordering
 
 
-class Kettlebell:
-    """
-    Класс гирь для занятий спортом
-    """
+#TODO Практика!
+"""
+Опишите класс Город, у которого есть следующие атрибуты:
+- название (name)
+- население (population)
 
-    def __init__(self, weight: int, color: str):
-        self.weight = weight
-        self.color = color
+Импортируйте декоратор total_ordering из модуля functools
+Используйте декоратор total_ordering для определения всех методов сравнения, описав
+лишь необходимый минимум
 
-    def __eq__(self, other):
-        """
-        Метод __eq__ определяет поведение оператора равенства ==.
-        :param other:
-        :return:
-        """
-        if isinstance(other, Kettlebell):
-            return self.weight == other.weight
-        return NotImplemented
+Опишите метод __str__ для вывода информации о городе в виде:
+"Город: <name>, Население: <population>"
 
-    def __ne__(self, other):
-        """
-        Метод __ne__ определяет поведение оператора неравенства !=.
-        :param other:
-        :return:
-        """
-        if isinstance(other, Kettlebell):
-            return self.weight != other.weight
-        return NotImplemented
-
-    def __lt__(self, other):
-        """
-        Метод __lt__ определяет поведение оператора "меньше" <.
-        :param other:
-        :return:
-        """
-        if isinstance(other, Kettlebell):
-            return self.weight < other.weight
-
-    def __le__(self, other):
-        """
-        Метод __le__ определяет поведение оператора "меньше или равно" <=.
-        :param other:
-        :return:
-        """
-        if isinstance(other, Kettlebell):
-            return self.weight <= other.weight
-        return NotImplemented
-
-    def __gt__(self, other):
-        """
-        Метод __gt__ определяет поведение оператора "больше" >.
-        :param other:
-        :return:
-        """
-        if isinstance(other, Kettlebell):
-            return self.weight > other.weight
-        return NotImplemented
-
-    def __ge__(self, other):
-        """
-        Метод __ge__ определяет поведение оператора "больше или равно" >=.
-        :param other:
-        :return:
-        """
-        if isinstance(other, Kettlebell):
-            return self.weight >= other.weight
-        return NotImplemented
-
-    def __str__(self):
-        """
-        Метод __str__ возвращает строковое представление объекта.
-        Используется для пользовательского вывода.
-        :return:
-        """
-        return f'Гиря. Цвет: {self.color}. Вес: {self.weight} кг.'
-
-
-# Создаем наследника дизайнерскую гирю (добавляем атрибут модель)
-
-class DesignerKettlebell(Kettlebell):
-    """
-    Класс дизайнерских гирь для занятий спортом
-    """
-
-    def __init__(self, weight: int, color: str, model: str):
-        super().__init__(weight, color)
-        self.model = model
-
-    def __str__(self):
-        """
-        Метод __str__ возвращает строковое представление объекта.
-        Используется для пользовательского вывода.
-        :return:
-        """
-        return f'Дизайнерская гиря. Модель: {self.model}. Цвет: {self.color}. Вес: {self.weight} кг.'
-
-
-# Создаем наследника дизайнерскую гирю (добавляем атрибут модель)
-d_kettlebell_1 = DesignerKettlebell(16, 'синий', 'Классическая')
-d_kettlebell_2 = DesignerKettlebell(32, 'синий', 'С перламутровыми пуговицами')
-d_kettlebell_3 = DesignerKettlebell(8, 'синий', 'Без пуговиц')
-
-
-d_kettelbells = [d_kettlebell_1, d_kettlebell_2, d_kettlebell_3]
-[print(kettlebell) for kettlebell in d_kettelbells]
-d_kettelbells.sort()
-[print(kettlebell) for kettlebell in d_kettelbells]
+Создайте несколько экземпляров класса Город и сравните их между собой
+"""
