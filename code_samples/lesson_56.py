ea572fff9e82ddeb8d253880cd3f03d05a47cb72
V <v@yandex.ru>
2023-12-19 22:37:23 +0600

    lesson_56: field - default, compare, repr, metadata

--- code_samples/lesson_56.py
+++ code_samples/lesson_56.py
@@ -7,28 +7,30 @@ Lesson 56 - Dataclasses
 - Атрибуты по умолчанию
 - Параметры декоратора dataclass
     - order=True - генерация дандер методов сравнения (по умолчанию генерируется только __eq__)
-"""
-from dataclasses import dataclass
 
-# TODO Практика!
-"""
-Опишите датакласс для описания книги.
-Книга имеет следующие атрибуты:
-- Название
-- Автор
-- Год издания
+- field - параметры для атрибутов
+-Параметры field:
+    - compare - участвует ли атрибут в сравнении
+    - default - значение по умолчанию
+    - repr - участвует ли атрибут в repr
+    - metadata - словарь с метаданными
+    - __dataclass_fields__['category'].metadata - метаданные конкретного атрибута
 
-Создайте 3 экземпляра класса книга.
-Создайте список книг.
-Сортируйте список книг.
 """
+from dataclasses import dataclass, field
 
 
 @dataclass(order=True)
 class Book:
     title: str
-    author: str
-    year: int
+    author: str = field(compare=False)
+    year: int = field(compare=False)
+    category: str = field(default='Без категории',  # значение по умолчанию
+                          compare=False,  # участвует ли атрибут в сравнении
+                          repr=False,  # участвует ли атрибут в repr
+                          # метаданные (могут быть использованы в других библиотеках)
+                          metadata={'marshmallow': {'load_only': True}}
+                          )
 
 
 book_1 = Book('Война и мир', 'Лев Толстой', 1869)
@@ -36,6 +38,8 @@ book_2 = Book('Преступление и наказание', 'Федор До
 book_3 = Book('Мастер и Маргарита', 'Михаил Булгаков', 1966)
 
 books = [book_1, book_2, book_3]
-books.sort()
-print(books)  # TypeError: '<' not supported between instances of 'Book' and 'Book'
-print(sorted(books, key=lambda book: book.year))
+
+print(book_1)
+
+# печатаем метадату - вероятно это никогда не пригодится, но пусть будет
+# print(book_1.__dataclass_fields__['category'].metadata)
