829055e6bf4e67b215cb9d440b8cc17fa8f6140b
V <v@yandex.ru>
2023-12-19 22:25:14 +0600

    lesson_56: решение практики

--- code_samples/lesson_56.py
+++ code_samples/lesson_56.py
@@ -10,8 +10,7 @@ Lesson 56 - Dataclasses
 """
 from dataclasses import dataclass
 
-
-#TODO Практика!
+# TODO Практика!
 """
 Опишите датакласс для описания книги.
 Книга имеет следующие атрибуты:
@@ -23,3 +22,20 @@ from dataclasses import dataclass
 Создайте список книг.
 Сортируйте список книг.
 """
+
+
+@dataclass(order=True)
+class Book:
+    title: str
+    author: str
+    year: int
+
+
+book_1 = Book('Война и мир', 'Лев Толстой', 1869)
+book_2 = Book('Преступление и наказание', 'Федор Достоевский', 1866)
+book_3 = Book('Мастер и Маргарита', 'Михаил Булгаков', 1966)
+
+books = [book_1, book_2, book_3]
+books.sort()
+print(books)  # TypeError: '<' not supported between instances of 'Book' and 'Book'
+print(sorted(books, key=lambda book: book.year))
