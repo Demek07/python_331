cc6fad86f30356ee03291312fd64d5b1d6fb13b3
V <v@yandex.ru>
2023-12-22 00:54:03 +0600

    lesson_58: пример вложенного класса

--- code_samples/lesson_58.py
+++ code_samples/lesson_58.py
@@ -6,79 +6,31 @@ Inner Classes
 21.12.2023
 
 """
-"""
-вот текстовая UML-диаграмма, описывающая отношения между object, type и пользовательским классом в Python:
-
-lua
-
-+------------+        +---------+
-|            |        |         |
-|    type    |<-------| MyClass |
-|            |        |         |
-+------+-----+        +---------+
-       ^
-       |
-+------+------+
-|             |
-|   object    |
-|             |
-+-------------+
-
-Объяснение:
-
-    type: Это метакласс в Python, от которого наследуются все классы, включая сам type. 
-    Он представляет собой тип всех классов и используется Python для создания новых классов.
-
-    object: Это базовый класс для всех объектов в Python. Все классы, включая type, 
-    неявно наследуются от object, обеспечивая базовый функционал объектов в Python.
-
-    MyClass: Это пользовательский класс, который, как и все классы в Python, неявно наследуется 
-    от type (потому что type — это метакласс для всех классов) и от object 
-    (потому что все классы являются объектами и наследуют базовый функционал).
-"""
-
-# Определение метакласса
-class AddHello(type):
-    def __init__(cls, name, bases, dct):
-        # Добавление метода hello к классу
-        cls.hello = lambda self: print("Привет от", self.__class__.__name__)
 
 
-# Определение класса с использованием метакласса AddHello
-class MyClass(metaclass=AddHello):
-    pass
+class BlogPost:
+    # Вложенный класс Meta для хранения метаданных
+    class Meta:
+        # Предположим, у нас есть некоторые параметры конфигурации
+        ordering = ['-date_published']
+        db_table = 'blog_post'
+
+    def __init__(self, title, content, date_published):
+        self.title = title
+        self.content = content
+        self.date_published = date_published
+
+    def save(self):
+        # Здесь мог бы быть код для сохранения экземпляра в базу данных
+        print(f"Saving '{self.title}' to table {self.Meta.db_table}")
+
+    @classmethod
+    def get_all_posts(cls):
+        # Здесь мог бы быть код для получения всех постов из базы данных
+        print(f"Getting all posts ordered by {cls.Meta.ordering[0]}")
 
 
-# Использование класса
-instance = MyClass()
-instance.hello()  # Выведет: Привет от MyClass
-
-
-# Определение метакласса
-class SimpleMeta(type):
-    """
-    Метакласс, который добавляет метод greet и атрибут greeting к классу.
-    Через переопределение метода __new__.
-    cls - класс, который создается
-    name - имя класса
-    bases - базовые классы
-    dct - атрибуты класса
-    """
-
-    def __new__(cls, name, bases, dct):
-        # Добавляем новый метод к dct
-        dct["greet"] = lambda self: print(f"Hello from {self.greeting}!")
-        # Добавляем новый атрибут к dct
-        dct["greeting"] = "SimpleClass"
-        # Создаем новый класс с помощью type.__new__
-        return super(SimpleMeta, cls).__new__(cls, name, bases, dct)
-
-
-# Определение класса с использованием метакласса SimpleMeta
-class MyClass(metaclass=SimpleMeta):
-    pass
-
-
-# Использование класса
-my_instance = MyClass()
-my_instance.greet()  # Вызывает метод, добавленный метаклассом
+# Создание и использование экземпляра BlogPost
+post = BlogPost("My First Post", "Hello, world!", "2021-01-01")
+post.save()  # Сохраняет пост, используя информацию из класса Meta
+BlogPost.get_all_posts()  # Получает все посты, используя порядок из класса Meta
