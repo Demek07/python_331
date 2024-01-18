bc6861b3455f2e5f660db97b5acbadf39c5fe66d
V <v@yandex.ru>
2024-01-19 00:56:42 +0600

    lesson_35: - Практический пример Фасад Прокси и marshmallow вместе

--- code_samples/lesson_35.py
+++ code_samples/lesson_35.py
@@ -7,98 +7,172 @@ Lesson 35
 - Facade (фасад)
 - Composite (композит)
 - Proxy (заместитель)
+- Практический пример использования паттернов Фасад + Прокси в погодном приложении
 """
-
-"""
-#### Цели и Задачи
-Паттерн "Прокси" в программировании предназначен для предоставления заместителя или плейсхолдера для другого объекта,
- чтобы контролировать доступ к нему. Основные задачи паттерна:
-
-- **Управление доступом**: Прокси может контролировать доступ к объекту, например, если объект требует защиты из-за 
-высоких ресурсных требований или по причинам безопасности.
-- **Отложенная инициализация (ленивая загрузка)**: Прокси может отложить создание объекта до тех пор, пока он 
-действительно не будет нужен, тем самым улучшая производительность.
-- **Логирование и аудит**: Прокси может записывать операции, выполняемые на объекте, для целей аудита или логирования.
-- **Контроль за ресурсами**: Прокси может управлять ресурсами, связанными с объектом, например, закрывать соединения
- с базой данных или освобождать память.
-
-#### Структура
-Паттерн прокси включает в себя следующие компоненты:
-
-1. **Субъект (Subject)**: Общий интерфейс для реального объекта и прокси. Это позволяет использовать прокси вместо
- реального объекта.
-2. **Реальный Объект (Real Object)**: Объект, для которого создается прокси. Это может быть объект, работа с которым 
-связана с большими ресурсными затратами или требует защиты.
-3. **Прокси (Proxy)**: Объект, который управляет доступом к реальному объекту. Он содержит ссылку на реальный объект
- и может управлять его созданием и удалением, а также предварительно обрабатывать или кэшировать запросы к нему.
+import time
 
 """
+Реализация погодного приложения. 
+Классы:
+- CurrentWeatherRequest - класс, который делает запрос к API погоды и возвращает текущую погоду в виде словаря
+- CurrentWeatherRequestProxy - класс, который хеширует запросы к API, и если запрос был выполнен в течении последних 
+10 минут, то возвращает сохраненный результат, а не делает новый запрос к API
+- CurrentWeatherFacade - класс, который оборачивает логику работы с API погоды и предоставляет простой интерфейс для
+- получения погоды
+- CurrentWeather - класс, который хранит в себе данные о погодe (датакласс)
+"""
+
+from dataclasses import dataclass
+from pprint import pprint
+from marshmallow import Schema, fields, post_load, INCLUDE, validate
+from marshmallow_jsonschema import JSONSchema
+import requests
+
+API_KEY = "23496c2a58b99648af590ee8a29c5348"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+TIME_FORMAT = "%d.%m.%Y %H:%M:%S"
+TIME_TRESHOLD = 30  #  30 секунд
+
+# Формируем Датакласс
+@dataclass
+class CurrentWeather:
+    city_name: str
+    weather_icon: str
+    description: str
+    temperature: float
+    feels_like: float
+    wind_speed: float
+    request_time = None
+
+    def __str__(self):
+        return f"Погода в городе {self.city_name}:\n" \
+               f"Температура: {self.temperature}°C\n" \
+               f"Ощущается как: {self.feels_like}°C\n" \
+               f"Скорость ветра: {self.wind_speed} м/с\n" \
+               f"Погодные условия: {self.description}"
 
 
-# Пример 1
-class Image:
-    def __init__(self, url):
-        self.url = url
-        self.image_data = None
 
-    def load_image(self):
-        self.image_data = f"Изображение загружено с {self.url}"
-        return self.image_data
+"""
+Marshmallow схема, которая проверяет только те поля,
+ которые нам нужны (все остальные игнорируются)
+ мы явно указываем местонахождение каждого поля в JSON
+ после чего, в @post_load мы указываем создаем экземпляр класса CurrentWeather
 
-
-class ImageProxy:
-    def __init__(self, url):
-        self.url = url
-        self._image = None
-
-    def load_image(self):
-        if not self._image:
-            self._image = Image(self.url)
-            return self._image.load_image()
-        return "Изображение уже загружено"
+мы допускаем что в JSON могут быть поля, которые нам не нужны на всех уровнях вложенности
+"""
 
 
-# Клиентский код
-proxy_image = ImageProxy("http://example.com/image.png")
-print(proxy_image.load_image())  # Загружает изображение
-print(proxy_image.load_image())  # Изображение уже загружено
+class CurrentWeatherSchema(Schema):
+    city_name = fields.Str(data_key='name')  # data_key - путь к данным в JSON
+    weather_icon = fields.Str(data_key='weather.0.icon')
+    description = fields.Str(data_key='weather.0.description')
+    # Проверяем температуру на диапазон от -70 +70 через range validate=validate.Range(min=18)
+    temperature = fields.Float(data_key='main.temp', validate=validate.Range(min=-10, max=70))
+    feels_like = fields.Float(data_key='main.feels_like')
+    wind_speed = fields.Float(data_key='wind.speed')
+
+    @post_load
+    def make_current_weather(self, data, **kwargs):
+        return CurrentWeather(
+            city_name=data['city_name'],
+            weather_icon=data['weather'][0]['icon'],
+            description=data['weather'][0]['description'],
+            temperature=data['main']['temp'],
+            feels_like=data['main']['feels_like'],
+            wind_speed=data['wind']['speed']
+        )
+
+    class Meta:
+        unknown = INCLUDE
 
 
-# Пример 2
+class CurrentWeatherRequest:
+    """
+    Класс для запроса погоды через API
+    """
 
-# Интерфейс для реального объекта и прокси
-class DataInterface:
-    def get_data(self):
-        raise NotImplementedError
+    def __init__(self, api_key: str = API_KEY, base_url: str = BASE_URL):
+        self.schema = CurrentWeatherSchema()
+        self.api_key = api_key
+        self.base_url = base_url
+        self.params = {
+            'q': None,
+            'lang': 'ru',
+            'units': 'metric',
+            'appid': api_key
+        }
+
+    def __call__(self, city_name: str) -> "CurrentWeather":
+        """
+        Метод для получения данных о погоде.
+        Возвращает валидированный экземпляр класса CurrentWeather
+        :param city_name: Название города
+        :return: Данные о погоде
+        """
+        self.params['q'] = city_name
+        response = requests.get(self.base_url, params=self.params)
+        if response.status_code == 200:
+            data = response.json()
+            return self.schema.load(data)
+        else:
+            raise Exception(f"Ошибка при запросе к API: {response.status_code}")
 
 
-# Реальный объект, содержащий конфиденциальную информацию
-class SensitiveData(DataInterface):
-    def get_data(self):
-        return "Конфиденциальные данные"
+class CurrentWeatherRequestProxy(CurrentWeatherRequest):
+    """
+    Класс для запроса погоды через API с использованием кэширования
+    Запросы кешируются в списке cache который хранит экземпляры
+    """
+
+    def __init__(self, api_key: str = API_KEY, base_url: str = BASE_URL):
+        super().__init__(api_key, base_url)
+        self.cache = []
+
+    def __call__(self, city_name: str) -> "CurrentWeather":
+        """
+        Метод для получения данных о погоде.
+        Возвращает валидированный экземпляр класса CurrentWeather
+        :param city_name: Название города
+        :return: Данные о погоде
+        """
+        for item in self.cache:
+            if item.city_name == city_name:
+                if time.time() - item.request_time < TIME_TRESHOLD:
+                    print(f"Данные о погоде в городе {city_name} получены из кэша\n"
+                          f"Время запроса: {time.strftime(TIME_FORMAT, time.localtime(item.request_time))}")
+                    return item
+        weather = super().__call__(city_name)
+        weather.request_time = time.time()
+        self.cache.append(weather)
+        print(f"Данные о погоде в городе {city_name} получены из API")
+        return weather
 
 
-# Прокси, контролирующий доступ к реальному объекту
-class DataProxy(DataInterface):
-    def __init__(self, data):
-        self.data = data
-        self.is_authenticated = False
+class CurrentWeatherFacade:
+    """
+    Класс для получения данных о погоде в городе
+    """
 
-    def authenticate(self, password):
-        if password == "секрет":
-            self.is_authenticated = True
+    def __init__(self, api_key: str = API_KEY, base_url: str = BASE_URL):
+        self.api_key = api_key
+        self.base_url = base_url
+        self.request = CurrentWeatherRequestProxy(api_key, base_url)
 
-    def get_data(self):
-        if self.is_authenticated:
-            return self.data.get_data()
-        return "Доступ запрещен"
+    def get_weather(self, city_name: str) -> "CurrentWeather":
+        """
+        Метод для получения данных о погоде.
+        Возвращает валидированный экземпляр класса CurrentWeather
+        :param city_name: Название города
+        :return: Данные о погоде
+        """
+        return self.request(city_name)
 
 
-# Клиентский код
-real_data = SensitiveData()
-proxy_data = DataProxy(real_data)
+if __name__ == '__main__':
+    weather_facade = CurrentWeatherFacade()
+    while True:
+        city = input("Введите название города: ")
+        weather = weather_facade.get_weather(city)
+        print(weather)
 
-print(proxy_data.get_data())  # Доступ запрещен
-
-proxy_data.authenticate("секрет")
-print(proxy_data.get_data())  # Конфиденциальные данные
