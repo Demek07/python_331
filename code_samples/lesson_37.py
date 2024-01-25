a127510945a45c101ccf0797f8ebaf78e16de8f5
V <v@yandex.ru>
2024-01-25 22:52:37 +0600

    lesson_36: generators StopIteration

--- code_samples/lesson_37.py
+++ code_samples/lesson_37.py
@@ -8,69 +8,16 @@ Lesson 37
 """
 from time import sleep
 
+# Однострочный генератор чисел до 3
+gen = (i for i in range(3))
+# print(len(gen)) # TypeError: object of type 'generator' has no len()
+# next() - возвращает следующий элемент генератора
 
-# Генераторы
-# Зачем нужны генераторы?
-
-# Этот код вызовет падение (нехватка ОЗУ)
-
-# Этот код попытается создать очень большой список чисел в памяти.
-# Если число n достаточно велико, это может вызвать ошибку из-за нехватки памяти.
-
-# n = 1000000000  # Очень большое число
-# large_list = list(range(n))  # Создание большого списка
-# print(sum(large_list))  # Суммирование элементов списка
-#
-
-# Этот код использует генератор для создания последовательности чисел.
-# Генератор производит числа по одному, не сохраняя всю последовательность в памяти.
-# Генераторы - это функции, которые возвращают объект-генератор (производят значения по одному за раз).
+while True:
+    try:
+        print(next(gen))
+    except StopIteration:
+        print("Генератор закончил работу")
+        break
 
 
-# def large_range(n):
-#     i = 0
-#     while i < n:
-#         yield i  # Генератор возвращает значение и "замораживает" своё состояние до следующего вызова
-#         i += 1
-
-
-# n = 1000000000  # Очень большое число
-# total = sum(large_range(n))  # Суммирование элементов генератора
-# print(total)
-
-# Однострочный вариант того и того
-# total_list = sum(range(n))
-# print(total_list)
-#
-# total_gen = sum(i for i in range(n))
-# print(total_gen)
-
-
-# Опишем бесконечный генератор, который будет возвращать от 0 до бесконечности
-# def infinite_sequence():
-#     num = 0
-#     while True:
-#         yield num
-#         num += 1
-
-
-# Запустим цикл, который будет выводить значения генератора
-# for i in infinite_sequence():
-#     print(i)
-#     sleep(1)
-
-# Генератор для построчного чтения txt файла
-# txt_file_path = "../data/generator_text.txt"
-#
-#
-# def read_lines_file(file_path):
-#     with open(file_path, "r", encoding='utf-8') as file:
-#         for line in file:
-#             yield line.strip()
-#
-#
-# generator = read_lines_file(txt_file_path)
-#
-# for line in generator:
-#     print(line)
-#
