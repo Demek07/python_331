186dbbf1ec46104be4f01cc2aa2b50d418a21e75
v <v@mail.ru>
2024-01-31 00:08:31 +0600

    lesson_38: pytests

--- code_samples/lesson_38.py
+++ code_samples/lesson_38.py
@@ -4,6 +4,13 @@ Pytest
 - pip install pytest
 - assert - assert, process, message
 - pytest (простой запуск тестов) - но нам надо передать файл, где искать тесты!
+- xfail - помечает тест как ожидаемо падающий
+- parametrize - позволяет передавать параметры в тестовую функцию
+- parametrize + xfail - позволяет передавать параметры в тестовую функцию и помечать тест как ожидаемо падающий
+- pytest.raises - проверяет, что в блоке with возбуждается исключение
+- fixtures - фикстуры, которые можно использовать в тестах
+- параметризация тестов с использованием фикстур
+- расположение тестов в проекте?
 """
 import pytest
 
@@ -99,7 +106,7 @@ def test_is_palindrome(string, result):
 # TODO: Параметризуйте тесты для функции check_game_rules
 
 test_data = [
-    ('Москва', 'Астрахань', False),
+    ('Москва', 'Астрахань', True),
     ('Астрахань', 'Новосибирск', False),
     ('Новосибирск', 'Красноярск', True),
     ('Красноярск', 'Калининград', True),
@@ -119,4 +126,73 @@ test_data = [
     ('Донецк', 'Киев', True),
     ('Киев', 'Воронеж', True),
     ('Воронеж', 'Житомир', True),
-    ]
+    pytest.param("Омск", "Санкт-Петербург", True,
+                 marks=pytest.mark.xfail(reason="Города не подходят")),
+    ('Житомир', 'Ростов', True),
+]
+
+
+@pytest.mark.parametrize('first_city, second_city, result', test_data)
+def test_check_game_rules(first_city, second_city, result):
+    assert check_game_rules(first_city, second_city) == result, "Города не подходят"
+
+
+def divide(a: int, b: int):
+    if b == 0:
+        raise ZeroDivisionError(f'Вы не можете поделить {a} на 0')
+    return a / b
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError) as exc_info:
+        divide(1, 0)
+    assert exc_info.type == ZeroDivisionError
+    assert str(exc_info.value) == 'Вы не можете поделить 1 на 0'
+
+
+# Фикстуры - фиксированные данные, которые можно использовать в тестах
+# похоже на домашние заготовки
+
+# pytest.fixture - позволяет создавать фикстуры
+
+# Опишем фикстуру, которая читает cities.json и возвращает список городов
+import json
+
+
+@pytest.fixture
+def cities():
+    with open('../data/cities.json', 'r', encoding='utf-8') as f:
+        cities = json.load(f)
+    return cities
+
+
+# Тестовая функция использует фикстуру cities
+# проверяет что cities - это список
+def test_is_cities_list(cities):
+    assert isinstance(cities, list), "cities не является списком"
+
+
+# # Тестовая функция использует фикстуру cities
+# # проверяет что cities[0]- это словарь
+def test_is_cities_list_dict(cities):
+    assert isinstance(cities[0], dict), "cities[0] не является словарем"
+
+
+# Проверяем что есть ключ name
+def test_is_key_name_in_cities(cities):
+    assert 'name' in cities[0], "В cities[0] нет ключа name"
+
+
+# Пробуем сделать тест с фикстурой + параметризацию
+# data = [index, содержимое lat, содержимое lon]
+data = [
+    (0, '52.65', '90.08333'),
+    (1, '53.71667', '91.41667'),
+    (2, '53.68333', '53.65'),
+]
+
+
+@pytest.mark.parametrize('index, lat, lon', data)
+def test_is_key_name_in_cities(index, lat, lon, cities):
+    assert cities[index]['coords']['lat'] == lat, "lat не совпадает"
+    assert cities[index]['coords']['lon'] == lon, "lon не совпадает"
