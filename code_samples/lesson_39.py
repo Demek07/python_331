dd8fa6ec623157d2c14346bbfad5df4fd7165fbb
v <v@mail.ru>
2024-02-02 00:42:48 +0600

    lesson_39: yield в переменной и в функции разбор после пары

--- code_samples/lesson_39.py
+++ code_samples/lesson_39.py
@@ -24,5 +24,28 @@ Lesson 39
 from random import randint as random
 import pytest
 
+txt_file_path = "../data/generator_text.txt"
 
 
+def read_lines_file(file_path):
+    with open(file_path, "r", encoding='utf-8') as file:
+        for line in file:
+            yield line.strip()
+
+
+generator = read_lines_file(txt_file_path)
+# # встречается ли слово тор в строке
+
+word = input('введите слово для поиска: ')
+if any(word.lower() in line.lower() for line in read_lines_file(txt_file_path)):
+    print(f"{word} есть в строках")
+else:
+    print(f"{word} нет в строках")
+
+spisok = list(line for line in generator if word.lower() in line.lower())
+print(spisok)
+print(len(spisok))
+
+spisok1 = list(line for line in read_lines_file(txt_file_path) if word.lower() in line.lower())
+print(spisok1)
+print(len(spisok1))
--- tests/hw_tests/hw_16_tests/hw_16_txt_handler_test.py
+++ tests/hw_tests/hw_16_tests/hw_16_txt_handler_test.py
@@ -14,25 +14,65 @@ from hw.hw_16 import TxtFileHandler
 import pytest
 import os
 
+#
+# @pytest.fixture(scope='module')
+# def txt_handler():
+#     txt_handler = TxtFileHandler('test.txt')
+#     yield txt_handler
+#     try:
+#         time.sleep(5)
+#         os.remove('test.txt')
+#     except FileNotFoundError:
+#         pass
+#
+#
+# def test_write_file(txt_handler):
+#     txt_handler.write_file([])
+#     assert os.path.exists('test.txt')
+#
+#
+# def test_append_file(txt_handler):
+#     txt_handler.append_file(['test'])
+#     with open('test.txt', 'r', encoding='utf-8') as file:
+#         assert file.read() == 'test'
+#
 
-@pytest.fixture(scope='module')
-def txt_handler():
-    txt_handler = TxtFileHandler('test.txt')
-    yield txt_handler
-    try:
-        time.sleep(5)
-        os.remove('test.txt')
-    except FileNotFoundError:
-        pass
+"""
+Эти же тесты в ООП стиле:
+Создаем класс TxtFileHandlerTest
+- Поле фикстуры для экземпляра класса TxtFileHandler = None
+- Метод setup_class, в котором создаем экземпляр класса TxtFileHandler
+- Метод teardown_class, в котором удаляем файл
+- Метод test_write_file, в котором вызываем метод write_file c пустым списком (проверяем, что файл создался)
+- Метод test_append_file, в котором вызываем метод append_file с данными (проверяем, что данные дописались в файл)
+"""
+
+import os
+from hw.hw_16 import TxtFileHandler
+import pytest
 
 
-def test_write_file(txt_handler):
-    txt_handler.write_file([])
-    assert os.path.exists('test.txt')
+class TestTxtFileHandler:
+    @classmethod
+    def setup_class(cls):
+        """Создание экземпляра класса TxtFileHandler."""
+        cls.txt_handler = TxtFileHandler('test.txt')
 
+    @classmethod
+    def teardown_class(cls):
+        """Удаление файла после выполнения всех тестов."""
+        try:
+            os.remove('test.txt')
+        except FileNotFoundError:
+            pass
 
-def test_append_file(txt_handler):
-    txt_handler.append_file(['test'])
-    with open('test.txt', 'r', encoding='utf-8') as file:
-        assert file.read() == 'test'
+    def test_write_file(self):
+        """Тестирование метода write_file с пустым списком."""
+        self.txt_handler.write_file([])
+        assert os.path.exists('test.txt')
 
+    def test_append_file(self):
+        """Тестирование метода append_file с добавлением данных."""
+        self.txt_handler.append_file(['test'])
+        with open('test.txt', 'r', encoding='utf-8') as file:
+            assert file.read() == 'test'
