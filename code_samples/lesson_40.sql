77ba7d15816775082092ac0a37f1aeef30087247
v <v@mail.ru>
2024-02-07 00:22:09 +0600

    lesson_40.sql

--- code_samples/lesson_40.sql
+++ code_samples/lesson_40.sql
@@ -1,6 +1,8 @@
 -- Lesson 40
 -- 06.02.2024
--- SELECT, FROM, WHERE, ORDER BY, LIMIT, OFFSET
+-- SELECT, FROM, WHERE, ORDER BY,
+-- IN, BETWEEN, LIKE,
+-- LIMIT, OFFSET
 -- Структура таблицы Marvel
 CREATE TABLE MarvelCharacters (
     id               INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -77,7 +79,55 @@ ORDER BY year, SEX DESC, name
 -- TOOD: практика!
 -- имя, год появления количество появлений девушки блондинки с голубыми глазами
 -- Blond Hair, Blue Eyes сортировка по году, и по количеству появлений
-
-
+SELECT name, year, APPEARANCES
+FROM MarvelCharacters
+WHERE HAIR = 'Blond Hair' AND EYE = 'Blue Eyes' AND SEX = 'Female Characters'
+AND APPEARANCES not null AND YEAR not null
+ORDER BY year DESC, APPEARANCES DESC
 -- Лысые злодеи! Bad Characters ALIGN и Bald Hair
 -- Сортировка по имени по возрастанию
+SELECT name, year, APPEARANCES
+FROM MarvelCharacters
+WHERE HAIR = 'Bald' AND ALIGN = 'Bad Characters'
+AND APPEARANCES not null AND YEAR not null
+ORDER BY name
+
+-- IN - входит в список
+-- Blond Hair, Strawberry Blond Hair, Red Hair
+SELECT name, hair
+FROM MarvelCharacters
+WHERE HAIR = 'Blond Hair' OR HAIR = 'Strawberry Blond Hair' OR HAIR = 'Red Hair'
+--WHERE HAIR IN ('Blond Hair', 'Strawberry Blond Hair', 'Red Hair')
+ORDER BY hair
+
+
+-- Получим персонажей с голубыми или зелеными глазами ИЛИ с рыжими или светлыми волосами
+SELECT name, hair, eye
+FROM MarvelCharacters
+WHERE HAIR OR EYE
+IN ('Blond Hair', 'Strawberry Blond Hair', 'Red Hair', 'Blue Eyes', 'Green Eyes')
+ORDER BY hair
+
+
+-- BETWEEN - между 1940 и 1945 годом где год и количество появлений не null
+-- Сортировка по году и количеству появлений
+SELECT name, year, APPEARANCES
+FROM MarvelCharacters
+WHERE YEAR BETWEEN 1940 AND 1945 AND (year AND APPEARANCES not null)
+ORDER BY year, APPEARANCES DESC
+
+-- Частота появления между 4043 и 200 раз где количество появлений не null
+-- Сортировка по количеству появлений
+SELECT name, year, APPEARANCES
+FROM MarvelCharacters
+WHERE APPEARANCES BETWEEN 200 AND 4043
+
+-- Уникальные значения - DISTINCT
+-- Уникальные значения пола
+SELECT DISTINCT SEX FROM MarvelCharacters
+
+-- LIMIT - ограничение количества строк
+-- OFFSET - смещение
+
+SELECT DISTINCT EYE FROM MarvelCharacters
+LIMIT 5 OFFSET 10
