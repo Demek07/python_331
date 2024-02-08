be2e3c610423f63e2c762ee8e1069f1948ad5592
v <v@mail.ru>
2024-02-09 00:03:31 +0600

    lesson_41.sql: подзапросы

--- code_samples/lesson_41.sql
+++ code_samples/lesson_41.sql
@@ -66,4 +66,68 @@ GROUP BY hair
 HAVING total > 50
 ORDER BY total DESC
 
--- TODO - Сделайте подобную операцию по identify
+-- AVG - среднее значение
+-- Сделаем групировку по FIRST_APPEARANCE и посчитаем среднее APPEARANCES
+SELECT FIRST_APPEARANCE month, AVG(APPEARANCES) avg_appearances
+FROM MarvelCharacters
+GROUP BY month
+ORDER BY month DESC
+
+-- MAX, MIN - максимальное и минимальное значение
+-- Максимальное количество появлений персонажа + имя персонажа
+SELECT name, FIRST_APPEARANCE month, MAX(APPEARANCES) max_appearances
+FROM MarvelCharacters
+GROUP BY month
+ORDER BY max_appearances DESC
+
+
+-- Минимальное количество появлений персонажа + имя персонажа
+SELECT name, FIRST_APPEARANCE month, MIN(APPEARANCES) min_appearances
+FROM MarvelCharacters
+GROUP BY month
+HAVING min_appearances > 5
+ORDER BY min_appearances DESC
+
+-- ROUND - округление
+-- Округлим среднее количество появлений персонажа до 2 знаков после запятой
+SELECT FIRST_APPEARANCE month, ROUND(AVG(APPEARANCES), 2) avg_appearances
+FROM MarvelCharacters
+GROUP BY month
+ORDER BY month DESC
+
+
+-- Максимальное количество появлений персонажа в каждом году + имя персонажа
+SELECT name Имя, year Год, MAX(appearances) Макс_появления
+FROM MarvelCharacters
+WHERE Год not null
+GROUP BY Год
+ORDER BY Год
+
+-- Подзапросы
+-- Вложенный запрос
+-- Выведем имена персонажей, у которых больше появлений, чем у персонажа с id 1
+SELECT name
+FROM MarvelCharacters
+WHERE APPEARANCES > (SELECT APPEARANCES FROM MarvelCharacters WHERE id = 1)
+
+-- Альтернатива группировке
+-- Подзапрос возвращает максимальное количество появлений персонажа в 1970 году
+-- Внешний запрос возвращает имя персонажа, год и количество появлений
+SELECT name AS Имя, Year AS Год, APPEARANCES AS Макс_появления
+FROM MarvelCharacters
+WHERE Year = 1970 AND
+APPEARANCES = (
+SELECT MAX(APPEARANCES)
+FROM MarvelCharacters
+WHERE Year = 1970
+)
+
+-- Подзапрос возвращает среднее количество появлений персонажа * 100
+-- Внешний запрос возвращает имя персонажа и количество появлений
+-- В итоге мы имеем персонажей, у которых количество появлений больше, чем среднее количество появлений * 100
+SELECT name, appearances
+FROM MarvelCharacters
+WHERE appearances > (
+SELECT AVG(appearances) * 100
+FROM MarvelCharacters
+);
