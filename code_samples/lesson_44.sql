17cdb52996a953704e9511c4b75150f96fe79eae
v <v@mail.ru>
2024-02-20 23:28:40 +0600

    lesson_44.sql созданы таблицы и записи для отношений многие ко многим

--- code_samples/lesson_44.sql
+++ code_samples/lesson_44.sql
@@ -22,6 +22,10 @@ CREATE TABLE Teachers (
     -- Другие поля, связанные с учителем
 );
 
+-- Таблица связи
+-- В ней могло бы быть гораздо больше данных, например
+-- дата начала и окончания обучения, оценки, дополнительные занятия, аудитории, отзывы и т.д.
+
 CREATE TABLE StudentsTeachers (
     student_id INTEGER,
     teachers_id INTEGER,
@@ -33,3 +37,38 @@ CREATE TABLE StudentsTeachers (
         ON DELETE CASCADE
         ON UPDATE NO ACTION
 );
+
+-- Добавим студентов
+INSERT INTO Students (name, last_name)
+VALUES ('Иван', 'Николаев'),
+       ('Петр', 'Сидоров'),
+       ('Сидор', 'Петров');
+       ('Анна', 'Иванова');
+       ('Светлана', 'Никифорова');
+       ('Дарья', 'Леонтьева');
+
+-- Добавим учителей
+INSERT INTO Teachers (name, last_name)
+VALUES ('Сигимунд', 'Фрейд'),
+       ('Карл', 'Ясперс'),
+       ('Мартин', 'Хайдеггер');
+       ('Фридрих', 'Ницше');
+       ('Альберт', 'Камю');
+       ('Жан-Поль', 'Сартр');
+
+-- Добавим связи.
+-- Что в нашем случае они будут представлять?
+-- Вероятно проведенное занятие!
+
+--INSERT INTO StudentsTeachers (student_id, teachers_id)
+--VALUES (1, 1),
+--       (2, 2),
+--       (3, 3),
+--       (4, 4),
+--       (5, 5),
+--       (6, 6);
+
+-- Добавим одну запись, найдя данные по имени и фамилии студента, и имени и фамилии препода
+INSERT INTO StudentsTeachers (student_id, teachers_id)
+VALUES ((SELECT id FROM Students WHERE name = 'Иван' AND last_name = 'Николаев'),
+        (SELECT id FROM Teachers WHERE name = 'Сигимунд' AND last_name = 'Фрейд'));
