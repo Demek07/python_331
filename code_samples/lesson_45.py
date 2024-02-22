2765c2b5da916e056fcfdc4dddf7eedecd518969
v <v@mail.ru>
2024-02-22 21:42:10 +0600

    lesson_45: init

---
+++ code_samples/lesson_45.py
@@ -0,0 +1,11 @@
+"""
+Lesson 45
+22.02.2024
+
+1. Знакомство с Sqlite3
+2. import sqlite3
+3. Основные объекты sqlite3
+- Objects: Connection, Cursor
+4. Основные методы объектов sqlite3
+- Methods: connect(), cursor(), executescript(), execute(), fetchone(), fetchall()
+"""
